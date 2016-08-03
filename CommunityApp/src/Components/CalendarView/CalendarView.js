9ede909965557da5a0ad377c7d7e163986500f46
Nick Dieff <Dieff@Nicks-MacBook-Pro.local>
2016-08-03 02:39:17 -0400

    fixed a boo-boo

--- CommunityApp/src/Components/CalendarView/CalendarView.js
+++ CommunityApp/src/Components/CalendarView/CalendarView.js
@@ -26,13 +26,10 @@ class CalendarView extends Component{
   render(){
     return(
       <View style = {{flex: 1}}>
-        
+
         <Label navigator = {this.props.navigator}/>
 
-        <View style = {{height: 55}}>
-        <Label/>
-        </View>
-        
+
     	  <ScrollView style = {{flex: 10, backgroundColor: "green"}}>
     		  <Day navigator = {this.props.navigator} dayOfWeek = "Friday" dayNum = {1} event = "7 pm: OTHS Football Game" event2 = "10 pm: Waterfront Fireworks"/>
     		  <Day navigator = {this.props.navigator} dayOfWeek = "Saturday" dayNum = {2} event = "10 am: Riverfest Parade" event2 = "12 am: OTHS Cross Country Meet"/>
